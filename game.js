280241ff60808a6f43397ba23cf2b777485acbe8
Michael Morfessis <Michael.Morfessis@globalinformationsystems.com>
2017-10-05 05:50:11 -0500

    Finished Psychic Game and Added Psuedocode for Hangman

--- game.js
+++ game.js
@@ -31,7 +31,35 @@ var computerRandom = computerChoices[Math.floor(Math.random() * computerChoices.
 //This is to ensure it worked..
 console.log(computerRandom)
 
+var slice1 = computerChoices.slice(0,13);
+var slice2 = computerChoices.slice(14,26);
+
+
+//created to different functions that do 2 different things when using the button 
+
+function firstHalf() {
+
+	document.getElementById("myBtn").onclick = function fun() {
+
+		var targetDiv = document.querySelector("#guesses");
+		var hint1 = document.createElement("div")
+		hint1.innerHTML = "The answer is between the letters A and M"
+		targetDiv.appendChild(hint1)
+
+	}
+}
+
+function secondHalf() {
+
+	document.getElementById("myBtn").onclick = function fun1() {
+
+		alert("The answer is between the letters N and Z")
+	}
+}
+
+
 //this allows for on pressing any key to relate it to the random computer choice chosen	
+
 
 document.onkeyup = function(event) {
 
@@ -39,17 +67,20 @@ document.onkeyup = function(event) {
 
 
 
-guessesSoFar.push(String.fromCharCode(event.keyCode).toLowerCase());
+	guessesSoFar.push(String.fromCharCode(event.keyCode).toLowerCase());
+
 
 //if statement allows playerguess to relate to computerchoice
 
-//if (playerChoices === playerGuess) {
+if (playerChoices.includes(playerGuess)) {
+
 	if (playerGuess === computerRandom) {
-		alert("You guessed correctly!")
-		wins++
-		resetGame()
-		console.log(guessesSoFar)
-		console.log(guesses)
+		alert("You guessed correctly!");
+		wins++;
+		resetGame();
+		console.log(guessesSoFar);
+		console.log(guesses);
+
 	}
 
 	else if ((playerGuess != computerRandom) && (guesses > 1)) {
@@ -58,10 +89,33 @@ guessesSoFar.push(String.fromCharCode(event.keyCode).toLowerCase());
 		console.log(guessesSoFar)
 		console.log(guesses)
 		document.querySelector('#guessesSoFar').innerHTML = "<p> Guesses So Far: " + guessesSoFar.join(", ") + "</p>"
-	
+		document.querySelector("#guesses").innerHTML = "<p> Guesses Left: " + guesses + "</p>"
+
+
+		if (guesses < 5) {
+			console.log("Would you like a hint??");
+			var targetDiv = document.querySelector("#guesses");
+			var hint = document.createElement("button");
+			hint.innerHTML = ("Press the button for a hint!<br>");
+			hint.setAttribute("class", "btn btn-primary")
+			hint.setAttribute("id", "myBtn")
+			targetDiv.appendChild(hint);
+
+
+			if (slice1.includes(computerRandom)) {
+
+				firstHalf()
+			} else {
+
+				secondHalf()
+			}
+		}
+
 	}
+
+
 	else {
-		alert("You Lost!!");
+		alert("You Lost!! The answer was " + computerRandom);
 		guesses--;
 		losses++
 		resetGame()
@@ -69,18 +123,19 @@ guessesSoFar.push(String.fromCharCode(event.keyCode).toLowerCase());
 		console.log(guesses)
 
 	}
-//}else {
-	//alert("I said.. choose a letter!")
-//}
 
 	var gameUpdate =
 	"<p> Wins: " + wins + "</p>" +
-	"<p> Losses: " + losses + "</p>" +
-	"<p> Guesses Left: " + guesses + "</p>"
+	"<p> Losses: " + losses + "</p>"
 
 
-document.querySelector("#game").innerHTML = gameUpdate
+	document.querySelector("#game").innerHTML = gameUpdate
+}else {
+	alert("I said.. choose a letter!")
 }
+
+}
+
 
 
 
